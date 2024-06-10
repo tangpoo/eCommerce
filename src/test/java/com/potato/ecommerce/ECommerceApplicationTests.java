76bd2cafefde724a08f551dba60c8f807b31785a
tangpoo <lhinav3@gmail.com>
2024-06-10 16:13:46 +0900

    :recycle: ci 오류 수정

--- src/test/java/com/potato/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/potato/ecommerce/ECommerceApplicationTests.java
@@ -3,7 +3,7 @@ package com.potato.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ECommerceApplicationTests.class)
 class ECommerceApplicationTests {
 
     @Test
